80df0b5435ad67700791bcdfac15c63d3b6e6d07
Martin Horyna <martinhoryna1@gmail.com>
2019-01-30 17:12:21 +0100

    jQ krok dale

--- js/init.js
+++ js/init.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
     $(".popup").click(function () {
-       alert("ANO")
+        $(".popupalert").fadeToggle(".popupalert");
     })
-    $("form").submit(function () {
+    $("form").submit(function (e) {
+        var form = $(this);
+        var error = 0;
+        console.log($(this).find(".req"));
+
         $(this).find(".req").each(function () {
-            
+            console.log($(this).val()== "");
+            if ($(this).val() == ""){
+                console.log("bar is empty")
+                e.preventDefault();
+            }
         })
     })
 })
---
+++ what.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["surname"]; ?>
+Your age is: <?php echo $_POST["age"]; ?>
+Your sex is: <?php echo $_POST["sex"]; ?>
+
+</body>
+</html>
